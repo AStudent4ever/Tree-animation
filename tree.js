6406929e5ae54a41d04c49ce4bbc31697d3a8490
AStudent4ever <120913735+AStudent4ever@users.noreply.github.com>
2023-09-06 00:14:47 +0300

    Update tree.js

--- tree.js
+++ tree.js
@@ -3,13 +3,13 @@ let ctx = canvas.getContext("2d");
 
 let angleIncrement = (30 * Math.PI) / 180;
 let startX = canvas.width / 2;
-let startY = canvas.height - 120;
-let height = (canvas.height * 7) / 24;
+let startY = canvas.height;
+let height = (canvas.height * 8) / 24;
 
-let thickness = 2;
-let maxDepth = 8;
+let thickness = 1;
+let maxDepth = 10;
 let count = 0;
-let branchPropagation = 5;
+let branchPropagation = 4;
 let createRect = (x, y, width, height, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
@@ -17,7 +17,7 @@ let createRect = (x, y, width, height, color) => {
 
 let drawLine = (x1, y1, x2, y2, thickness, color) => {
   ctx.lineWidth = thickness;
-  ctx.strokeStyle = color;
+  ctx.strokeStyel = color;
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
@@ -29,7 +29,7 @@ let drawBranch = (x, y, height, thickness, angle, depth) => {
   let endX = x - height * Math.sin(angle);
   let endY = y - height * Math.cos(angle);
 
-  drawLine(x, y, endX, endY, thickness, "orange");
+  drawLine(x, y, endX, endY, thickness, "black");
 
   let newHeight = (height * 8) / 12;
   let newThickness = (thickness * 2) / 3;
@@ -54,11 +54,5 @@ let drawBranch = (x, y, height, thickness, angle, depth) => {
   }
 };
 
-let drawTree = () => {
-  createRect(0, 0, canvas.width, canvas.height, "#000000");
-  angleIncrement -= 0.02;
-  drawBranch(startX, startY, height, thickness, 0, Math.PI / 2);
-  requestAnimationFrame(drawTree);
-};
-
-drawTree();
+createRect(0, 0, canvas.width, canvas.height, "#EEEEEE");
+drawBranch(startX, startY, height, thickness, 0, Math.PI / 2);
